428493e97a782fb0b9766d2567411312e476d5bd
Priyasa Santra <84921052+PriyasaSantra@users.noreply.github.com>
2021-07-07 20:46:24 +0530

    Add files via upload

---
+++ CONCATENATE.java
@@ -0,0 +1,13 @@
+
+//CONCATENATION OF DNA1 AND DNA2
+public class CONCATENATE{
+    public static void main(String[] args){
+        String DNA1="ATATATAT";
+        String DNA2="GCGCGC";
+        
+        System.out.println("The value of DNA1 is: "+ DNA1);
+        System.out.println("The value of DNA2 is: "+ DNA2);
+        System.out.println("The concatenation of DNA1 and DNA2 is: "+ DNA1+DNA2);
+         }
+}
+
