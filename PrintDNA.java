c0d2b45fb7130871fa96369732328940ba085293
Priyasa Santra <84921052+PriyasaSantra@users.noreply.github.com>
2021-07-07 20:41:42 +0530

    Add files via upload

---
+++ PrintDNA.java
@@ -0,0 +1,12 @@
+//	CREATE A VARIABLE ‘DNA’ AND STORE “ATACGATACAA” AND PRINT DNA
+
+
+public class PrintDNA{
+    public static void main (String[]args)
+    {
+        String DNA= "ATACGATACAA";
+        System.out.print("The DNA sequence is: "+DNA);
+        
+        
+    }
+}
