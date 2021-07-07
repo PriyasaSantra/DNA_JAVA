60bf4bc12d86b25513f2ab40100c7271f6b9f1f4
Priyasa Santra <84921052+PriyasaSantra@users.noreply.github.com>
2021-07-07 20:44:48 +0530

    Add files via upload

---
+++ DNAbase.java
@@ -0,0 +1,14 @@
+//	CREATE A DNA BASE TABLE
+public class DNAbase{
+    public static void main(String[] args)
+    {
+       System.out.println("\t \t \t DNA BASE");
+       System.out.println("----------------------------------------------------------------------------");
+       System.out.println("\t Code \t \t Description \t \t Complement");
+       System.out.println("-----------------------------------------------------------------------------");
+       System.out.println("\t A \t \t Adenine \t \t \t T");
+       System.out.println("\t T \t \t Thymine \t \t \t A");
+       System.out.println("\t G \t \t Guanine \t \t \t C");
+       System.out.println("\t C \t \t Cytosine \t \t \t G");
+         }
+}
